b4eeb36ca54a3811600e7d9cf0d3a69be1fda06f
sophia <your_email@youremail.com>
2019-07-06 16:20:04 +0900

    코끼리 서비스 프론트엔드 초기설정완료

---
+++ src/Pages/Main/Main.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import './Main.sass'
+
+
+
+class Main extends React.Component{
+    render(){
+        return(
+            <div>
+                안녕하세요 코끼리 홈페이지입니다.
+            </div>
+        )
+
+    }
+}
+
+
+
+export default Main;
